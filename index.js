b4cb6f7d5e571fed47887f610fc5231e6ed5c33f
John K. Speck <speckworks@gmail.com>
2020-05-26 21:54:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+class Node{
+  constructor(data, left = null, right = null){
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BST{
+  constructor(){
+    this.root = null;
+  }
+  add(data){
+    const node = this.root;
+    if(this.node === null){
+      this.root = new Node(data);
+      return
+    }else{
+      const searchTree = function(node){
+        if (data < node.data){
+          if(node.left === null){
+            
+
+          }
+        }
+
+      }
+    }
+
+  }
+
+
+}
