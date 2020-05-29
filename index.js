f0af468b9a9a561c692a3fe1c2263b56c74f9860
John K. Speck <speckworks@gmail.com>
2020-05-29 14:35:14 +0000

    fixed bug on line 15

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-class Node{
-  constructor(data, left = null, right = null){
+class Node {
+  constructor(data, left = null, right = null) {
     this.data = data;
     this.left = left;
     this.right = right;
   }
 }
 
-class BST{
-  constructor(){
+class BST {
+  constructor() {
     this.root = null;
   }
-
-  add(data){
+  add(data) {
     const node = this.root;
-    if(this.node === null){
+    if(this.root === null){
       this.root = new Node(data);
       return;
-    }else{
+    } else {
       const searchTree = function(node){
         if (data < node.data){
           if(node.left === null){
@@ -32,13 +31,13 @@ class BST{
             }else if(node.right !== null){
               return searchTree(node.right);
             }
-        }else{
-            return null
+        } else {
+            return null;
           }
-          };
-        return searchTree(node);
-          }
-        }
+      };
+      return searchTree(node);
+    } 
+  }
   findMin(){
         let current = this.root;
         while (current.left ==! null){
@@ -120,13 +119,13 @@ class BST{
 
 const bst = new BST();
 bst.add(4);
-bst.add(2);
-bst.add(6);
-bst.add(1);
-bst.add(3);
-bst.add(5);
-bst.add(7);
-bst.remove(4);
-console.log(findMind());
-console.log(findMax());
-console.log(bst.isPresent(4));
+// bst.add(2);
+// bst.add(6);
+// bst.add(1);
+// bst.add(3);
+// bst.add(5);
+// bst.add(7);
+// bst.remove(4);
+// console.log(findMin());
+// console.log(findMax());
+// console.log(bst.isPresent(4));
